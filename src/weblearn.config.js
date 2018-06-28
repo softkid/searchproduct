ba435b9c223d87a4dad16da21f34981d067245b3
softkid <softkids@naver.com>
2018-06-28 13:01:46 +0900

    git push

--- src/weblearn.config.js
+++ src/weblearn.config.js
@@ -1,31 +1,31 @@
 const env = (process.env.NODE_ENV === 'production') ? require('../.env.production') : require('../.env.local')
 const baseUri = 'https://www.googleapis.com/youtube/v3'
 const apiKey = env.YOUTUBE_API_KEY
-const title = 'Web Learn: it\'s like YouTube Gaming, but for Web Developers'
+const title = 'Search Textile: it\'s like YouTube Product, but for Buyers and Supplier'
 const tweet = encodeURIComponent(title)
 
 export default {
   app: {
-    name: 'Web Learn',
-    url: 'https://freearhey.github.io/web-learn/',
+    name: 'Search Textile',
+    url: 'http://www.searchtextile.com',
     title: title,
-    description: 'Web Learn is an open source service which provides simple access to thousands of video lessons on web developing and programming.',
-    author: 'Arhey',
+    description: 'Search Textile is an free service which provides simple access to thousands of video products on textile.',
+    author: 'Stanly',
     logo: {
       normal: 'build/images/logo.svg',
       mini: 'build/images/logo-mini.svg',
       share: 'build/images/weblearn-banner.png'
     },
     github: {
-      username: 'freearhey',
-      repo: 'freearhey/web-learn'
+      username: 'softkids',
+      repo: 'softkids/searchproduct'
     },
     twitter: {
-      username: 'arhey',
+      username: 'softkid',
       tweet: tweet
     },
     links: [
-      '<a class="github-button" href="https://github.com/freearhey/web-learn" data-count-href="/freearhey/web-learn/stargazers" data-count-api="/repos/freearhey/web-learn#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star freearhey/web-learn on GitHub">Star</a>',
+      '<a class="github-button" href="https://github.com/softkid/searchproduct" data-count-href="/softkid/searchproduct/stargazers" data-count-api="/repos/softkid/searchproduct#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star softkid/searchproduct on GitHub">Star</a>',
       '<a class="twitter-follow-button" href="https://twitter.com/arhey">Follow @arhey</a>',
       '<a class="twitter-share-button" data-via="arhey" href="https://twitter.com/intent/tweet?text=' + tweet + '">Tweet</a>'
     ]

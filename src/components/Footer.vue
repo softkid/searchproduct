b04a834655ff3db6fd439dfe703d801dc2166aef
softkid <softkids@naver.com>
2018-07-08 13:24:51 +0900

    git push

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,16 +2,7 @@
   footer.footer
     .container
       .content.has-text-centered
-<<<<<<< HEAD
-        p Designed by #[a(:href="'https://github.com/' + app.github.username",target="_blank") {{ app.author }}]. Licensed under #[a(href="http://opensource.org/licenses/MIT") MIT].
-        p.social-buttons
-          span(v-for="link in app.links",v-html="link")
-        p
-          a.icon(:href="'https://github.com/' + app.github.repo",target="_blank")
-            i.ion-social-github
-=======
         p Designed by #[ {{ app.author }} ]  Licensed SEARCH TEXTILE 
->>>>>>> 87904cfcc6c599c6286699bba58afcaad7d7e8ed
 </template>
 
 <script>
